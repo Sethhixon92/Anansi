7e3dd1915384942dbd9711299aa07a0a3aa2e11b
Good Vibes <sethhixon1992@gmail.com>
2021-11-16 20:10:01 -0600

    added timesheet seeds

--- seeds/index.js
+++ seeds/index.js
@@ -1,7 +1,9 @@
 const seedsEmployees = require('./employees-seeds');
 const seedsComment = require('./comment-seeds');
+const seedsTimesheet = require('./timesheet-seeds');
 
 const sequelize = require('../config/connection');
+const { Sequelize } = require('sequelize/types');
 
 const seedAll = async () => {
     await sequelize.sync({ force: true });
@@ -12,5 +14,10 @@ const seedAll = async () => {
     await sequelize.sync({ force: true });
     console.log('\n----- DATABASE SYNCED -----/n');
     await seedsComment();
+    console.log('\n----- DATABASE SYNCED -----/n');
+
+    await sequelize.sync({ force: true });
+    console.log('\n----- DATABASE SYNCED -----/n');
+    await seedsTimesheet();
     console.log('\n----- DATABASE SYNCED -----/n');
 }
