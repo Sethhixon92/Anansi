dcf3ef8c4ac9832fcf997c85e97a46870c5620ad
Good Vibes <sethhixon1992@gmail.com>
2021-12-05 13:54:02 -0600

    updated seeds

--- seeders/index.js
+++ seeders/index.js
@@ -1,5 +1,6 @@
 const seedEmployees = require('./employee-seeds');
 const seedComments = require('./comment-seeds');
+const seedsTimesheet = require('./timesheet-seeds');
 const pkg = require('../../package.json');
 const sequelize = require('../config/connection');
 
@@ -7,10 +8,10 @@ const seedAll = async () => {
     await sequelize.sync({ force: true });
     console.log('\n----- DATABASE SYNCED -----/n');
   
-    await seedsEmployees();
+    await seedEmployees();
     console.log('\n----- EMPLOYEES SEEDED -----/n');
 
-    await seedsComment();
+    await seedComments();
     console.log('\n----- COMMENT SEEDED -----/n');
 
     await seedsTimesheet();
